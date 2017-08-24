c04b2343a6905a2ddfeaca922a4bfc9aee66a2c9
J. F. Zhang <zhangjianfei3@gmail.com>
2017-08-24 16:02:38 +0800

    Windows 10/Ubuntu 16.04均可用
    使用watchdog监控文件变化
    删除不必要的代码

--- qiniu4blog/qiniu4blog.py
+++ qiniu4blog/qiniu4blog.py
@@ -1,130 +1,86 @@
 #!/usr/bin/env python3
 
-import os, time, datetime, platform, urllib, hashlib
-import qiniu
-from mimetypes import MimeTypes
-import pyperclip
-from os.path import expanduser
 import configparser
+import datetime
+import hashlib
+import os
+import platform
+import sys
+import time
+import urllib
+from mimetypes import MimeTypes
 
-homedir = expanduser("~")
+import pyperclip
+import qiniu
+from watchdog.events import PatternMatchingEventHandler
+from watchdog.observers import Observer
+
 config = configparser.ConfigParser()
-config.read(homedir+'/qiniu.cfg')
+config.read(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'qiniu.cfg'))
+bucket = config.get('config', 'bucket')
+ak = config.get('config', 'accessKey')
+sk = config.get('config', 'secretKey')
+path_to_watch = config.get('config', 'path_to_watch', fallback='.')
+addr = config.get('config', 'addr')
+style = config.get('config', 'style', fallback='')  # 图片样式
 mime = MimeTypes()
 now = datetime.datetime.now()
 
 
-try:
-    bucket = config.get('config', 'bucket')
-    accessKey = config.get('config', 'accessKey')
-    secretKey = config.get('config', 'secretKey')
-    path_to_watch = config.get('config', 'path_to_watch')
-    enable = config.get('custom_url','enable')
-    if enable == 'false':
-        print('custom_url not set')
-    else:
-        addr = config.get('custom_url','addr')
+class MyHandler(PatternMatchingEventHandler):
+    patterns = ["*.jpeg", "*.jpg", "*.png", "*.bmp", "*.gif", "*.tiff", "*.tar.gz", "*.zip"]
+    ignore_directories = True
+    case_sensitive = False
+    count = 0
+
+    def on_created(self, event):
+        # 不知为何新建文件会触发两次通知事件, 于是偷懒加了计数器, 达到只传一次的目的
+        if self.count % 2:
+            go(event.src_path)
+        self.count += 1
 
 
-except ConfigParser.NoSectionError as err:
-    print('Error Config File:' + err)
+def go(fp):
+    # watchdog太过灵敏, 需要设定一点延时, 不然文件还未写入磁盘就开始后续操作会报permission deny的错误
+    time.sleep(0.5)
+    name, remote_key = generate_key(fp)  # 生成远端存储别名
+    upload(fp, remote_key)  # 上传
+    src = '![' + name + ']' + '(' + addr + remote_key + style + ')'
+    set_clipboard(src)  # 整理md图片链接格式到剪贴板
 
 
-def setcodeingbyos():
-    '''获取系统平台,设置编解码'''
-    if 'cygwin' in platform.system().lower():
-        code = 'GBK'
-    elif os.name == 'nt' or platform.system() == 'Windows':
-        code = 'GBK'
-    elif os.name == 'mac' or platform.system() == 'Darwin':
-        code = 'utf-8'
-    elif os.name == 'posix' or platform.system() == 'Linux':
-        code = 'utf-8'
-    return  code
+def set_clipboard(s):
+    pyperclip.copy(s)
+    pyperclip.paste()
+    print(s)
 
 
-def set_clipboard(url_list):
-	for url in url_list:
-		pyperclip.copy(url)
-	spam = pyperclip.paste()
+def upload(fp, uploadname):
+    auth = qiniu.Auth(ak, sk)
+    token = auth.upload_token(bucket, key=None)
+    qiniu.put_file(token, uploadname, fp, mime_type=mime.guess_type(fp)[0])
 
 
-
-def parseRet(retData, respInfo):
-    '''处理上传结果'''
-    if retData != None:
-        print("Upload file success!")
-        print("Hash: " + retData["hash"])
-        print("Key: " + retData["key"])
-        for k, v in retData.items():
-            if k[:2] == "x:":
-                print(k + ":" + v)
-        for k, v in retData.items():
-            if k[:2] == "x:" or k == "hash" or k == "key":
-                continue
-            else:
-                print(k + ":" + str(v))
-    else:
-        print("Upload file failed!")
-        print("Error: " + respInfo.text_body)
-
-
-def upload_without_key(bucket, filePath, uploadname):
-    '''上传文件'''
-
-    auth = qiniu.Auth(accessKey, secretKey)
-    upToken = auth.upload_token(bucket, key=None)
-    
-    key = uploadname
-    retData, respInfo = qiniu.put_file(upToken, key, filePath, mime_type=mime.guess_type(filePath)[0])
-    parseRet(retData, respInfo)
-
-
-def getkey(filename):
-    ext = filename[filename.rfind('.'):]
-    file_path = path_to_watch + '/' + filename
-    md5 = hashlib.md5(open(file_path, 'rb').read()).hexdigest()
+def generate_key(fp):
+    name, ext = os.path.splitext(os.path.basename(fp))
+    with open(fp, 'rb') as fh:
+        md5 = hashlib.md5(fh.read()).hexdigest()
     # remote url: filetype/year/month/md5.filetype
     remote = ext[1:] + '/' + str(now.year) + '/' + str(now.month) + '/' + md5 + ext
-    return remote
+    return name, remote
 
-
-def main():
-    print("running ... ...")
-    before = dict([(f, None) for f in os.listdir(path_to_watch)])
-    while 1:
-        time.sleep(1)
-        after = dict([(f, None) for f in os.listdir(path_to_watch)])
-        added = [f for f in after if not f in before]
-        removed = [f for f in before if not f in after]
-        if added:
-            print("Added Files: " + ", ".join(added))
-            # print(added)
-            url_list = []
-
-            for i in added:
-                filekey = getkey(i)
-
-                upload_without_key(bucket, os.path.join(path_to_watch, i), filekey)
-                if enable == 'true':
-                    url = addr + urllib.parse.quote(filekey)
-                else:
-                    url = 'http://' + bucket + '.qiniudn.com/' + urllib.parse.quote(filekey)
-                url_list.append(url)
-
-            with open('image_markdown.txt', 'a') as f:
-                for url in url_list:
-                    image = '![' + added[0] + ']' + '(' + url + ')' + '\n'
-                    f.write(image)
-            print("image url [markdown] is save in image_markdwon.txt")
-
-            set_clipboard(url_list)
-        if removed:
-            print("Removed Files: " + ", ".join(removed))
-            print(removed)
-        before = after
 
 if __name__ == "__main__":
-    main()
-
-
+    path = sys.argv[1] if len(sys.argv) > 1 else path_to_watch
+    event_handler = MyHandler()
+    observer = Observer()
+    observer.schedule(event_handler, path)
+    observer.start()
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+    finally:
+        print("总共上传了{}个文件".format(event_handler.count // 2))
+    observer.join()
